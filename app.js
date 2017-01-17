160d4eb6901cd704dd50abd1886824cde8fa9231
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-17 11:19:50 -0600

    added ng-repeat method to display list items in the DOM

--- app.js
+++ app.js
@@ -27,8 +27,12 @@ app.controller('MainCrtl', function($scope){
 })
 
 // -------------------- LIST --------------------------
-app.controller('ListCtrl', function($scope){
-
+app.controller('ListCtrl', function($scope, $http){
     console.log("here is the list controller");
 
+    $http.get(`list.json`)
+    .then(function(val) {
+        console.log(val.data.list);
+        $scope.list = val.data.list
+    })
 })
